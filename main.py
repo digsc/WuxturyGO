e5885af7ee26bc6686475b8bc9fe56df4fcb6315
Daniel Brachman <91846017+digsc@users.noreply.github.com>
2022-10-28 14:14:35 -0700

    Lets start this project.
    Did basically nothing

---
+++ main.py
@@ -0,0 +1,6 @@
+print("WuxturyGO - MINECRAFT ACCOUNT CHECKER")
+print("COPYRIGHT 2022 FEAROFSNOW LLC")
+
+email = input("Please enter you Mojang accounts email:")
+password = input("Please enter your Mojang accounts password:")
+
